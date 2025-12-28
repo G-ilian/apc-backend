f24681b9e27f4b25bacc3355183cbeae95eb2370
G-ilian <gbiel.boza@gmail.com>
2025-12-28 03:12:39 -0300

    feat: add product type and measurement unit enums

---
+++ src/common/enums/product.enum.ts
@@ -0,0 +1,13 @@
+export enum ProductType {
+  WOOL = 'WOOL',
+  MILK = 'MILK',
+  CHEESE = 'CHEESE',
+  SUPPLY = 'SUPPLY',
+  OTHER = 'OTHER',
+}
+
+export enum MeasurementUnit {
+  KG = 'KG',
+  LITER = 'LITER',
+  UNIT = 'UNIT',
+}
